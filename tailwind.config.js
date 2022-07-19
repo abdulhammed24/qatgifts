7f4e4a22bbf71dd4f96a961954d173b6f6e58113
abdulhammed24 <abdlhamd3@gmail.com>
2022-07-19 05:45:46 -0700

    new

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "light-pink": "#fbf5f3",
+        purple: "#9b3994",
+        "slate-gray": "#708090",
+      },
+    },
+  },
+  plugins: [],
+};
